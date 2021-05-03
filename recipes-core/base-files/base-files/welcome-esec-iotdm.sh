5a2b70602791f059d3cb12603c0d7b69ac767550
Maik Otto <m.otto@phytec.de>
2021-05-03 12:48:46 +0000

    recipes-core: base-files: Add welcome-esec-iotdm after login
    The script checks the onboarding state and shows instructions for the
    user.
    
    Signed-off-by: Maik Otto <m.otto@phytec.de>

---
+++ recipes-core/base-files/base-files/welcome-esec-iotdm.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+echo "Welcome to your PHYTEC Board with support"
+echo "for the ESEC IoT Device Manager Platform"
+echo "OnBoarding state:"
+if [ $(/usr/bin/jq .awsclient < /mnt/config/aws/config/esec.config | grep start | wc -l) -eq 0 ]; then
+    echo "NOT yet onboarded"
+    echo "Please start the phytec-board-config tool first!"
+else
+    echo "Successfully onboarded"
+fi
