140c881332c780f96744f4d82d694c7bbc9ddbc8
Maik Otto <m.otto@phytec.de>
2021-05-25 12:24:11 +0000

    recipes-core: base-files: Rename welcome-esec-iotdm to welcome-connagtive
    Signed-off-by: Maik Otto <m.otto@phytec.de>

--- recipes-core/base-files/base-files/welcome-connagtive.sh
+++ recipes-core/base-files/base-files/welcome-connagtive.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo "Welcome to your PHYTEC Board with support"
-echo "for the ESEC IoT Device Manager Platform"
+echo "for the Connagtive IoT Device Suite Platform"
 echo "OnBoarding state:"
 if [ $(/usr/bin/jq .awsclient < /mnt/config/aws/config/esec.config | grep start | wc -l) -eq 0 ]; then
     echo "NOT yet onboarded"
