ff2216b0acc6e89d72dcc132e2ac2ee37a0303b2
Martin Schwan <m.schwan@phytec.de>
2021-06-21 08:48:45 +0200

    base-files: Add check in welcome-connagtive and prettify
    Add a check in welcome-connagtive.sh script that displays the onboarding
    state to prevent referencing non-existing files before the first
    provisioning.
    
    Also change the formatting and add some colors for a better visual
    experience.
    
    Signed-off-by: Martin Schwan <m.schwan@phytec.de>

--- recipes-core/base-files/base-files/welcome-connagtive.sh
+++ recipes-core/base-files/base-files/welcome-connagtive.sh
@@ -1,10 +1,21 @@
 #!/bin/sh
-echo "Welcome to your PHYTEC Board with support"
-echo "for the Connagtive IoT Device Suite Platform"
-echo "OnBoarding state:"
-if [ $(/usr/bin/jq .awsclient < /mnt/config/aws/config/esec.config | grep start | wc -l) -eq 0 ]; then
-    echo "NOT yet onboarded"
-    echo "Please start the phytec-board-config tool first!"
+
+WELCOME_TEXT="
+Welcome to your PHYTEC Board with support for the Connagtive IoT Device Suite!
+Onboarding state:"
+
+print_not_onboarded() {
+	echo -e "$WELCOME_TEXT \e[0;31mNOT ONBOARDED\e[0m (Please start \"phytec-board-config\" first)\n"
+}
+
+print_successfully_onboarded() {
+	echo -e "$WELCOME_TEXT \e[0;32mSUCCESSFULLY ONBOARDED\e[0m\n"
+}
+
+if [ ! -e /mnt/config/aws/config/esec.config ]; then
+	print_not_onboarded
+elif [ $(/usr/bin/jq .awsclient < /mnt/config/aws/config/esec.config | grep start | wc -l) -eq 0 ]; then
+	print_not_onboarded
 else
-    echo "Successfully onboarded"
+	print_successfully_onboarded
 fi
